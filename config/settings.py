f646132fe10d70dfecf5503beb8f41c02e51a6be
Rafał Kondziela <hajtpl@gmail.com>
2021-12-13 19:33:41 +0100

    Add DRF

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3th party libs
+    "rest_framework",
     # Own modules
     'api.event',
     'api.ticket',
